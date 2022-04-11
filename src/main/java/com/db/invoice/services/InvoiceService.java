9d69a1a499daa482a29e2a605c444fe6659fc7b8
Razvan <razvan.maftei@yahoo.com>
2022-04-11 19:53:20 +0300

    added logic implemenatation for updateInvoice method in service package

--- src/main/java/com/db/invoice/services/InvoiceService.java
+++ src/main/java/com/db/invoice/services/InvoiceService.java
@@ -1,10 +1,14 @@
 package com.db.invoice.services;
 
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import com.db.invoice.models.Invoice;
 import com.db.invoice.repos.InvoiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.sound.midi.Soundbank;
+import java.util.Optional;
 
 @Service
 public class InvoiceService {
@@ -22,6 +26,25 @@ public class InvoiceService {
         return dbInvoice;
     }
 
-    public void updateInvoice(Long invoiceId, Invoice invoice) {
+    public Invoice fetchInvoice(Long invoiceId){
+        Optional<Invoice> optionalInvoice= invoiceRepository.findById(invoiceId);
+        if(optionalInvoice.isPresent()){
+            Invoice invoice =  optionalInvoice.get();
+            return invoice;
+        }else{
+            System.out.println("No such file in database: " + invoiceId);
+            return null;
+        }
+        //to do exceptions
+    }
+    public Invoice updateInvoice(Long invoiceId, Invoice inputInvoice) {
+        Invoice dbInvoice=fetchInvoice(invoiceId);
+        if(dbInvoice!=null){
+            dbInvoice.setAmount(inputInvoice.getAmount());
+            dbInvoice.setNumber(inputInvoice.getNumber());
+
+            return invoiceRepository.save(dbInvoice);
+        }
+        return null;
     }
 }
